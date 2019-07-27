0de5b6a26af954bcb9ed8539434a7004103f4a2a
Ethan B <eebmagic@gmail.com>
2019-07-27 19:03:57 -0400

    Edited to comlpy with pep8

--- trim.py
+++ trim.py
@@ -3,21 +3,25 @@ import sys
 import os
 from pathlib import Path
 
+
 def getMostFreq(inputList):
 	frequencies = {}
 	for i, val in enumerate(inputList):
 		frequencies[val] = frequencies.get(val, 0) + 1
 	return sorted(frequencies, key=frequencies.get, reverse=True)[0]
 
+
 def topPos(inputList, ignorableItem):
 	for i, point in enumerate(inputList):
 		if point != ignorableItem:
 			return i
 
+
 def bottomPos(inputList, ignorableItem):
 	for i, point in enumerate(inputList[::-1]):
 		if point != ignorableItem:
 			return len(inputList) - i
+
 
 def leftPos(inputList, size, ignorableItem):
 	for x in range(size[0]):
@@ -26,15 +30,18 @@ def leftPos(inputList, size, ignorableItem):
 			if inputList[index] != ignorableItem:
 				return index
 
+
 def rightPos(inputList, size, ignorableItem):
 	for x in range(size[0]):
 		for y in range(size[1]):
-			index = (y * size[0]) + (size[0]-x-1)
+			index = (y * size[0]) + (size[0] - x - 1)
 			if inputList[index] != ignorableItem:
 				return index
 
+
 def makeArray(inputList, size):
-	return [inputList[i:i+size[0]] for i in range(0, len(inputList), size[0])]
+	return [inputList[i:i + size[0]] for i in range(0, len(inputList), size[0])]
+
 
 def makeFlat(inputArray):
 	output = []
@@ -42,15 +49,16 @@ def makeFlat(inputArray):
 		output = output + row
 	return output
 
+
 ###########################################################################
-### SETTINGS ###
-PADDING = 5 
+# SETTINGS #
+PADDING = 5
 SQUARE = False
 OUTPUT_PATH = str(Path.home()) + "/Desktop/CROPPED.png"
 ###########################################################################
 
-##### START #####
-### GET INPUT IMAGE ###
+# START #
+# GET INPUT IMAGE #
 if len(sys.argv) > 1:
 	IMAGE_PATH = sys.argv[1].strip()
 	print(IMAGE_PATH)
@@ -63,20 +71,20 @@ if "-s" in sys.argv or "--s" in sys.argv:
 paddOptions = ["-p", "--p", "--padding"]
 for option in paddOptions:
 	if option in sys.argv:
-		PADDING = int(sys.argv[sys.argv.index(option)+1])
+		PADDING = int(sys.argv[sys.argv.index(option) + 1])
 
-### LOAD DATA ###
+# LOAD DATA #
 im = Image.open(IMAGE_PATH)
 originalData = list(im.getdata())
 
-### SIMPLIFY DATA ###
-#make searchData
+# SIMPLIFY DATA #
+# make searchData
 searchData = []
 for point in originalData:
-	newTup = [round(x-(x%30), -1) for x in point[:3]]
+	newTup = [round(x - (x % 30), -1) for x in point[:3]]
 	searchData.append(tuple(newTup))
 
-### FIND EDGE INDEXES ###
+# FIND EDGE INDEXES #
 background = getMostFreq(searchData)
 top = topPos(searchData, background)
 bottom = bottomPos(searchData, background)
@@ -85,13 +93,13 @@ right = rightPos(searchData, im.size, background)
 
 # Convert Edge pixels to Rows/Collumns #
 edge = {
-	"left": left%im.size[0],
-	"right": right%im.size[0]+1,
-	"top": int(top/im.size[0]),
-	"bottom": int(bottom/im.size[0]+1)
+	"left": left % im.size[0],
+	"right": right % im.size[0] + 1,
+	"top": int(top / im.size[0]),
+	"bottom": int(bottom / im.size[0] + 1)
 }
 
-### TRIM ###
+# TRIM #
 asArray = makeArray(originalData, im.size)
 
 # Adjust Edges to make square
@@ -102,24 +110,28 @@ if SQUARE:
 
 	if width == height:
 		pass
-	
+
 	elif min(width, height) == height:
-		edge["bottom"] += difference/2
-		edge["top"] -= difference/2
-	
+		edge["bottom"] += difference / 2
+		edge["top"] -= difference / 2
+
 	elif min(width, height) == width:
-		edge["left"] -= difference/2
-		edge["right"] += difference/2
+		edge["left"] -= difference / 2
+		edge["right"] += difference / 2
 
 
 selection = []
 for row in asArray:
-	selection.append(row[int(edge["left"]-PADDING):int(edge["right"]+PADDING)])
-selection = selection[edge["top"]-PADDING:edge["bottom"]+PADDING]
+	left_edge = int(edge["left"] - PADDING)
+	right_edge = int(edge["right"] + PADDING)
+	selection.append(row[left_edge:right_edge])
+selection = selection[edge["top"] - PADDING:edge["bottom"] + PADDING]
 
 
-### SAVE ###
-newSize = (int(edge["right"]-edge["left"]+(PADDING*2)), int(edge["bottom"]-edge["top"]+(PADDING*2)))
+# SAVE #
+new_x = int(edge["right"] - edge["left"] + (PADDING * 2))
+new_y = int(edge["bottom"] - edge["top"] + (PADDING * 2))
+newSize = (new_x, new_y)
 newIm = Image.new("RGB", newSize)
 newIm.putdata(makeFlat(selection))
 newIm.save(OUTPUT_PATH)
